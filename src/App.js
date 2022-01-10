8cc1562a38e39d34a2eca2f9f7444b66f1ecdc7b
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2022-01-10 22:00:51 +0100

    ADD: Finishing Routes, start Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { routes } from "./helper/routes";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         {routes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} />
         ))}
+        <Route path='/' exact element={<Dashboard />} />
+        <Route path='/*' element={<Navigate to='/' />} />
       </Routes>
     </BrowserRouter>
   );
