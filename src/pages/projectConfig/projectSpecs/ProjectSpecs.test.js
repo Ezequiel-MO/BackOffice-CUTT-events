604cd110c350a0c205aa3daecd420cd10175bea8
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2022-01-11 14:54:49 +0100

    minor fix

--- src/pages/projectConfig/projectSpecs/ProjectSpecs.test.js
+++ src/pages/projectConfig/projectSpecs/ProjectSpecs.test.js
@@ -5,7 +5,7 @@ describe("form", () => {
 
 describe("submit", () => {
   it("v- should make sure Code is not duped in the DB", () => {});
-  it("if the code is Duped, it should send a message", () => {});
-  it("should inject captured fields in the DB", () => {});
-  it("should dispatch Project Status to the Active Code", () => {});
+  it("v-if the code is Duped, it should send a message", () => {});
+  it("v-should inject captured fields in the DB", () => {});
+  it("v-should dispatch Project Status to the Active Code", () => {});
 });
