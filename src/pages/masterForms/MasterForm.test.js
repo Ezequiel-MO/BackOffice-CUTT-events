c6946277979e1cd25ae66c9aaea946ffaf461fba
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2022-01-12 16:25:13 +0100

    minor fix

---
+++ src/pages/masterForms/MasterForm.test.js
@@ -0,0 +1,15 @@
+describe("form", () => {
+  it("v-Should render a form with General vendor data", () => {});
+  it("v-Should capture vendor input text, text-area, boolean, and number fields, and store them in an object", () => {});
+  it("v-Should capture vendor images", () => {});
+  it("v- On Submit -Should append text and images to a formData object", () => {});
+  it("On Submit, - check that the vendor is not already registered in the dB", () => {});
+  it("v- On Submit, - if vendor is not already in the DB, should post all info to endpoint", () => {});
+  it("v- On Submit, - check that other forms are not in the DB - ex: HCC Montblanc, vs vendor Montblanc. If coincidence, ask for permission before proceeding", () => {});
+  it("v- On Submit, - if vendor is already in the DB, should alert user", () => {});
+  it("if post status is 201, it should navigate back to dashboard", () => {});
+});
+
+describe("form-validations", () => {
+  it("form should validate that the coordinates are valid numbers - in decimal format", () => {});
+});
