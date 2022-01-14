f9cc161eac0772ef0833e8fa9334012d9146352e
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2022-01-14 09:57:49 +0100

    ADD: Post hotel and hotelRates to project

--- src/ui/tabs/Tabs.js
+++ src/ui/tabs/Tabs.js
@@ -1,42 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import HotelRatesTable from "../../components/projectConfig/HotelRatesTable/HotelRatesTable";
 import { selectHotelRates } from "../../features/HotelRatesSlice";
-import { checkVendorIsUnique } from "../../helper/HelperFunctions/HelperFunctions";
+import { PostToEndpoint } from "../../helper/HelperFunctions/HelperFunctions";
+import { useAxiosFetch } from "../../hooks/useAxiosFetch";
+import { baseURL } from "../../helper/axios";
+import { selectProjectStatus } from "../../features/ProjectStatusSlice";
 import "./tabStyles.css";
 
 const Tabs = ({ hotels }) => {
   const [visibleTab, setVisibleTab] = useState(hotels[0]._id);
+  const [hotelRates, setHotelRates] = useState(hotels);
   const hotelsArr = useSelector(selectHotelRates);
+  const projectStatus = useSelector(selectProjectStatus);
+  const {
+    data: { project: projectByCode },
+  } = useAxiosFetch(`${baseURL}/project/${projectStatus}`);
 
-  const listTitles = hotels.map((hotel) => (
-    <>
-      {checkVendorIsUnique("_id", hotel._id, hotelsArr) && (
-        <li
-          key={hotel._id}
-          onClick={() => setVisibleTab(hotel._id)}
-          className={
-            visibleTab === hotel._id
-              ? "tab-title tab-title--active"
-              : "tab-title"
-          }
-        >
-          {hotel.name}
-        </li>
-      )}
-    </>
+  useEffect(() => {
+    if (hotelRates.length === 0) {
+      PostToEndpoint(hotelsArr, `/addHotels/${projectByCode._id}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hotelRates]);
+
+  useEffect(() => {
+    const filteredHotels = hotels?.filter((hotel) => {
+      return !hotelsArr.find((hotelArr) => hotelArr._id === hotel._id);
+    });
+    setVisibleTab(filteredHotels.length > 0 ? filteredHotels[0]._id : null);
+    setHotelRates(filteredHotels);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hotelsArr]);
+
+  const listTitles = hotelRates.map((hotel) => (
+    <li
+      key={hotel._id}
+      onClick={() => setVisibleTab(hotel._id)}
+      className={
+        visibleTab === hotel._id ? "tab-title tab-title--active" : "tab-title"
+      }
+    >
+      {hotel.name}
+    </li>
   ));
 
-  const listContent = hotels.map((hotel) => (
+  const listContent = hotelRates.map((hotel) => (
     <div
       key={hotel._id}
-      style={visibleTab === hotel._id ? {} : { display: "none" }}
+      className={
+        visibleTab === hotel._id
+          ? "tab-content"
+          : "tab-content tab-content--hidden"
+      }
     >
-      {checkVendorIsUnique("_id", hotel._id, hotelsArr) && (
-        <HotelRatesTable hotel={hotel} />
-      )}
+      <HotelRatesTable hotel={hotel} />
     </div>
   ));
+
+  if (hotelRates.length === 0) return <h1>Thanks for submitting</h1>;
 
   return (
     <div className='tabs'>
